3947101cc091671c6f635c42e1e20e190a75ddcb
Augustoiacopini <adiacopini@gmail.com>
2022-02-23 23:53:46 -0300

    mensaje.py
    objeto para mandar mensaje via e-mail con la imagen detectada

--- version 1.1.0/mensaje.py
+++ version 1.1.0/mensaje.py
@@ -4,17 +4,17 @@ class email:
         self.usuario = usuario
         self.imagen = imagen
 
-        # Importamos librerías
+
         import os
         import smtplib
         import mimetypes
 
-        # Importamos los módulos necesarios
+ 
         from email.mime.multipart import MIMEMultipart
         from email.mime.image import MIMEImage
         from email.encoders import encode_base64
 
-        # Creamos objeto Multipart, quien será el recipiente que enviaremos
+     
         msg = MIMEMultipart()
         msg['From']="lolatay298@gmail.com"
         msg['To']=self.usuario
@@ -23,22 +23,20 @@ class email:
 
 
 
-        # Adjuntamos Imagen
+     
         file = open(self.imagen, "rb")
         attach_image = MIMEImage(file.read())
         attach_image.add_header('Content-Disposition', 'attachment; filename = "avatar.png"')
         msg.attach(attach_image)
 
-        # Autenticamos
+     
         mailServer = smtplib.SMTP('smtp.gmail.com',587)
         mailServer.ehlo()
         mailServer.starttls()
         mailServer.ehlo()
-        mailServer.login("lolatay298@gmail.com","Tierra01")
+        mailServer.login("xxxxx@gmail.com","******")
 
-        # Enviamos
-        mailServer.sendmail("lolatay298@gmail.com", self.usuario, msg.as_string())
-
-        # Cerramos conexión
+     
+        mailServer.sendmail("xxxxxxxx@gmail.com", self.usuario, msg.as_string())
         mailServer.close()
         
